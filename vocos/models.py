d1e317640933606063238ca4c419a43d4c4d3b18
Stefano <stefano.sc.ciapponi@gmail.com>
2024-01-10 12:09:42 +0100

    modified:   vocos/models.py

--- vocos/models.py
+++ vocos/models.py
@@ -27,9 +27,9 @@ class Backbone(nn.Module):
 
 class XiVocosBackboneFixedChannels(Backbone):
 
-    def __init__(self, freqs, dim = 128, num_layers = 8):
+    def __init__(self, input_channels, dim = 128, num_layers = 8):
         super().__init__()
-        self.first_layer = XiConv(c_in=freqs, c_out=dim, kernel_size=(3,1), pool=1, skip_res=None, skip_tensor_in=False)
+        self.first_layer = XiConv(c_in=input_channels, c_out=dim, kernel_size=(3,1), pool=1, skip_res=None, skip_tensor_in=False)
         self.net = nn.ModuleList(
             [
                 XiConv(c_in=dim, c_out=dim, kernel_size=3, pool=1, skip_res=None, skip_tensor_in=False)
