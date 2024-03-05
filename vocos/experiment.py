778408164d0660614deaf63a18019a03a2e599c3
Stefano <stefano.sc.ciapponi@gmail.com>
2024-03-05 10:16:30 +0100

    modified:   vocos/experiment.py

--- vocos/experiment.py
+++ vocos/experiment.py
@@ -198,6 +198,8 @@ class VocosExp(pl.LightningModule):
         audio_input = batch
         audio_hat = self(audio_input, **kwargs)
 
+        print(audio_hat.shape)
+        print(audio_hat)
         audio_16_khz = torchaudio.functional.resample(audio_input, orig_freq=self.hparams.sample_rate, new_freq=16000)
         audio_hat_16khz = torchaudio.functional.resample(audio_hat, orig_freq=self.hparams.sample_rate, new_freq=16000)
 
