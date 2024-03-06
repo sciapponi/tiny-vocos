bbdb08abaace3e7e3e611d68bdca5ce89896d0ac
Stefano <stefano.sc.ciapponi@gmail.com>
2024-03-06 18:02:57 +0100

    modified:   inference.py

--- inference.py
+++ inference.py
@@ -37,7 +37,7 @@ for idx, conf in enumerate(model_configs):
         spec = fe(y)
         # print(spec)
         vocos_output = model.decode(spec)[0]
-        # print(vocos_output.shape)
+        print(vocos_output)
         # Upsample to 44100 Hz for better reproduction on audio hardware
         vocos_output = torchaudio.functional.resample(vocos_output, orig_freq=24000, new_freq=44100).cpu()
         torchaudio.save(f"audio/{idx}_{i}.mp3", vocos_output, 44100, compression=128)
