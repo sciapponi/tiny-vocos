17d750dcf057c49ed23d0ceae2049c3c3ee85883
Stefano <stefano.sc.ciapponi@gmail.com>
2024-03-06 17:41:13 +0100

    modified:   inference.py 	modified:   vocos/pretrained.py

--- vocos/pretrained.py
+++ vocos/pretrained.py
@@ -112,9 +112,9 @@ class Vocos(nn.Module):
             Tensor: The output tensor representing the reconstructed audio waveform of shape (B, T).
         """
         x = self.backbone(features_input, **kwargs)
-        print(x.shape)
+        # print(x.shape)
         audio_output = self.head(x)
-        print(audio_output)
+        # print(audio_output)
         return audio_output
 
     @torch.inference_mode()
